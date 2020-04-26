9ef5cb287299541b31328f33d9d869ba4c2dc684
Thanh Nguyen Xuan <rainlove9876@gmail.com>
2020-04-26 16:01:30 +0700

    remove file thua

--- src/redux/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-import myReducer from "./reducers";
-import { status, sort } from "./actions";
-const store = createStore(myReducer)
-
-console.log(store.dispatch(sort({
-  by: 'status',
-  value: -1
-})), 'store');
